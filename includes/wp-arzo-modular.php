226e70fd4c5017774b276e6da907d85c445760a9
Yasir Shabbir <yasiranjan007@gmail.com>
2026-01-26 03:46:49 +0500

    refactored the Plugin Manager.

--- includes/wp-arzo-modular.php
+++ includes/wp-arzo-modular.php
@@ -18,17 +18,9 @@ if ($action === 'wp_arzo_standalone') {
     $action = 'info';
 }
 
-// Include the header (everything before switch statement - lines 1-1464)
-include(WP_ARZO_PLUGIN_DIR . 'includes/wp-arzo-header.php');
-
-// Route to modular feature files
-$feature_files = [
-    'info' => 'site-info.php',
-    'users' => 'users.php',
-    'database' => 'database.php',
-    'files' => 'files.php',
-    // Others will be added one by one
-];
+// Handle login actions before any output
+$login_message = '';
+$login_redirect = '';
 
 // Check for file download or AJAX file operations which need to run before headers if they happen to be in a feature file
 if (isset($_GET['tab'])) {
@@ -44,7 +36,27 @@ if (isset($_GET['tab'])) {
             exit;
         }
     }
+    if ($_GET['tab'] === 'plugins' && isset($_GET['operation']) && $_GET['operation'] === 'get_plugins_page') {
+        if (file_exists(WP_ARZO_PLUGIN_DIR . 'features/plugins.php')) {
+            include(WP_ARZO_PLUGIN_DIR . 'features/plugins.php');
+            exit;
+        }
+    }
 }
+
+// Include the header (everything before switch statement - lines 1-1464)
+include(WP_ARZO_PLUGIN_DIR . 'includes/wp-arzo-header.php');
+
+// Route to modular feature files
+$feature_files = [
+    'info' => 'site-info.php',
+    'users' => 'users.php',
+    'database' => 'database.php',
+    'files' => 'files.php',
+    // Others will be added one by one
+];
+
+
 if (isset($feature_files[$action]) && file_exists(WP_ARZO_PLUGIN_DIR . 'features/' . $feature_files[$action])) {
     // Load modular feature
     include(WP_ARZO_PLUGIN_DIR . 'features/' . $feature_files[$action]);
